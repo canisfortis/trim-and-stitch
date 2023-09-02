1d124b7fb9845ecc6fc97773b53c74aa36944ba9
canisfortis <52336477+canisfortis@users.noreply.github.com>
2023-09-02 20:09:34 +1000

    Update New-TrimAndStitchVideo.ps1

--- New-TrimAndStitchVideo.ps1
+++ New-TrimAndStitchVideo.ps1
@@ -1,15 +1,26 @@
+# How to use: Execute each section of code together by selecting the lines of code between the Section X -- START and Section X -- END comments and pressing F8.
+
+
+### Section 1 -- START ###
+# This section of code will prompt you for the start and end timestamps of each video you want to trim. It will then trim the video and save it to a new folder.
+# It also creates a concat file that will be used in the next section to merge the trimmed videos together and save it to the root folder. It also sets the output file for the merged video.
 $date = get-date
 $fileNameDateString = $($date.ToString("yyyy-MM-dd-hh-mm-ss"))
 $gameRoot = "D:\ShadowPlay\recordings\Battlefield 2042"
-
 $theSourceRecordingsDirectory = "$gameRoot\temp"
 $trimedVideoOuputFolder = "$gameRoot\$fileNameDateString\temp_trimmed"
 $concatFile = "$gameRoot\$fileNameDateString\concatfile$($fileNameDateString).txt"
 $mergedVideo = "$gameRoot\Battlefield 2042 merged $($date.ToString("yyyy-MM-dd-hh-mm-ss")).mp4"
+### Section 1 -- END ###
 
+
+### Section 2 -- START ###
+# This section of code will import the source videos and save them to the $sourceVideos variable.
 $sourceVideos = Get-ChildItem $theSourceRecordingsDirectory -File -Filter "*.mp4"
+### Section 2 -- END ###
 
-
+### Section 3 -- START ###
+# This section creates the folders if they don't exist.
 if (!(Test-Path $theSourceRecordingsDirectory)){
     New-Item -Path $theSourceRecordingsDirectory -Type Directory
 }
@@ -17,7 +28,10 @@ if (!(Test-Path $theSourceRecordingsDirectory)){
 if (!(Test-Path $trimedVideoOuputFolder)){
     New-Item -Path $trimedVideoOuputFolder -Type Directory
 }
+### Section 3 -- END ###
 
+### Section 4 -- START ###
+# This section of code will prompt you for the start and end timestamps of each video you want to trim and save the data to the $videoTrimArray variable.
 $videoTrimArray = @()
 foreach ($sourceVideo in $sourceVideos) {
 
@@ -42,8 +56,10 @@ foreach ($sourceVideo in $sourceVideos) {
     Stop-Process $process.Id
 
 }
+### Section 4 -- END ###
 
-#pulls out the parts of the videos based timestamps collected previously
+### Section 5 -- START ###
+# This section of code will trim the videos and save them to the $trimedVideoOuputFolder. It will also create the concat file and merge the trimmed videos together and save it to the root folder. It also opens the merged video in VLC.
 $trimData = $videoTrimArray
 $counter = 0
 foreach ($trim in $trimData) {
@@ -54,15 +70,16 @@ foreach ($trim in $trimData) {
 }
 
 
-#stitches together the trimmed videos if there is more than one trimmed video, other rename the single trimmed video
-if ($trimData.Count -gt 1) {
 
-    ffmpeg -f concat -safe 0 -i $concatFile -c copy $mergedVideo 
+    if ($trimData.Count -gt 1) {
 
-}
-else {
-    Copy-Item $trimmedVideo -Destination $mergedVideo
-}
+        ffmpeg -f concat -safe 0 -i $concatFile -c copy $mergedVideo 
+
+    }
+    else {
+        Copy-Item $trimmedVideo -Destination $mergedVideo
+    }
 
 
 $process = [Diagnostics.Process]::Start("C:\Program Files\VideoLAN\VLC\vlc.exe", "`"$mergedVideo`"")
+### Section 5 -- END ###
